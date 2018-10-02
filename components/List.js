111319cf974e41ae9ad8b04aebffe83ec3ecfa18
Joe Previte <jjprevite@gmail.com>
2018-10-01 19:44:51 -0700

    create reusable components

---
+++ components/List.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default styled.ul`
+  margin-top: 0;
+  margin-left: 30px;
+  text-align: left;
+  width: 400px;
+  font-size: 39px;
+
+  li {
+    margin: 20px 0;
+  }
+`
